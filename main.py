9121943d11e46a2b94f5df393be983d8a7fc941d
Sayandip Halder <sayandiph4@gmail.com>
2021-10-30 23:06:56 +0530

    delete functionality and database

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 import requests
 from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
@@ -12,19 +15,30 @@ video_put_args.add_argument("likes", type=str, help="Likes of the video", requir
 
 videos = {}
 
-def abort_func(video_id):
+def abort_if_video_doesnt_exist(video_id):
     if video_id not in videos:
         abort(404, message="Invalid video id")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists...")
 
 
 class Video(Resource):
     def get(self, video_id):
-        abort_func(video_id)
+        abort_if_video_doesnt_exist(video_id)
         return videos[video_id]
+
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
-        return videos[video_id]
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,10 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
-print(response.json())
+data = [
+    {"likes": 20, "name": "John", "views": 100000},
+    {"likes": 0, "name": "Bill", "views": 100000},
+    {"likes": 100000, "name": "Tom", "views": 100000}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 
 input()
-
-response = requests.get(BASE + "video/1")
+response = requests.delete(BASE+"video/1")
+print(response)
+input()
+response = requests.get(BASE + "video/6")
 print(response.json())
