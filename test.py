d06c506ca8e2df4eaf7022fd00fb8263a387ada0
Sayandip Halder <sayandiph4@gmail.com>
2021-10-31 00:28:40 +0530

    final

--- test.py
+++ test.py
@@ -2,18 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [
-    {"likes": 20, "name": "John", "views": 100000},
-    {"likes": 0, "name": "Bill", "views": 100000},
-    {"likes": 100000, "name": "Tom", "views": 100000}
-]
-
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+response = requests.put(BASE + "video/0", {"likes": 20, "name": "John", "views": 100000})
+print(response.json())
 
 input()
-response = requests.delete(BASE+"video/1")
+response = requests.delete(BASE+"video/0")
 print(response)
 input()
 response = requests.get(BASE + "video/6")
